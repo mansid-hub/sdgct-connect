5be0f1721590e936bbb4d7d5839a6967219707f9
mansid-hub <mansid1100@gmail.com>
2026-05-25 12:26:53 +0530

    Update institutions.ts
    repositioned the profile image

--- src/data/institutions.ts
+++ src/data/institutions.ts
@@ -889,7 +889,8 @@ const prerna = schoolsCat?.items.find(
 if (prerna) {
   prerna.about = prernaAbout;
   prerna.image = prerna1;
-  prerna.imageScale = 0.85;
+  prerna.imageScale = 0.95;
+  prerna.imagePosition = "center center";
   prerna.gallery = [prerna1, prerna2, prerna3, prerna4, prerna5, prerna6, prerna7, prerna9, prerna10, prerna11, prerna12];
   prerna.highlights = [];
 }
